91f604499e2fd96e8c4e72bf681777b7472bbf3d
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-06 21:58:32 -0500

    arregla posiciones

--- main.js
+++ main.js
@@ -4,21 +4,27 @@ slec.setAttribute("class","card")
 
 let div = document.createElement("DIV");
 div.setAttribute("class","imgBx")
-slec.insertAdjacentElement("beforebegin",div)
+//slec.insertAdjacentElement("beforebegin",div)
+slec.appendChild(div);
 
 let img = document.createElement("IMG");
 img.setAttribute("src","fotoperfil.jpg");
 //div.insertAdjacentElement("beforebegin",img)
+div.appendChild(img)
 
 let divc = document.createElement("DIV");
 divc.setAttribute("class","content")
-slec.insertAdjacentElement("beforebegin",divc)
+//slec.insertAdjacentElement("beforebegin",divc)
+slec.appendChild(divc)
 
 let divd = document.createElement("DIV");
 divd.setAttribute("class","details")
-divc.insertAdjacentElement("beforebegin",divd)
+//divc.insertAdjacentElement("beforebegin",divd)
+divc.appendChild(divd);
 
 divd.insertAdjacentHTML("beforeend",`
-<h2>Alina Smith<br><span>Senior UX/UI Designerk</span></h2>
+<h2>Jhorman Felipe <br><span>Senior UX/UI Designerk</span></h2>
 
 `);
+
+console.log(slec);
